9114579aeb92ea8b24762050a6e30f7c826a4769
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2020-12-06 19:44:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import streamlit as st
-# from utils import process_tweet, lookup, naive_bayes_predict
+from utils import process_tweet, lookup, naive_bayes_predict
 
 
-st.write('a simpple app')
+def main():
+    #start the user interface
+    st.write("This is just a small text classification app. Type in your text below and don't forget to press the enter button before clicking/pressing the 'classify' button")
+    st.write("Don't fret if the prediction is not correct or if it is not what you expected, the model is not perfect.")
 
-# def main():
-#     #start the user interface
-#     st.write("This is just a small text classification app. Type in your text below and don't forget to press the enter button before clicking/pressing the 'classify' button")
-#     st.write("Don't fret if the prediction is not correct or if it is not what you expected, the model is not perfect.")
+    st.write("There is no provision for neutral text, yet...")
+    my_text = st.text_input("Enter the text you want to classify", "Change this...", max_chars=100, key='to_classify')
 
-#     st.write("There is no provision for neutral text, yet...")
-#     my_text = st.text_input("Enter the text you want to classify", "Change this...", max_chars=100, key='to_classify')
+    if st.button('Classify', key='classify_button'):
+        if naive_bayes_predict(my_text) > 0:
+            # the predicted class is 1
+            st.write('Your input text is positive')
+        else:
+            # otherwise the predicted class is 0
+            st.write('Your input text is negative')
 
-#     if st.button('Classify', key='classify_button'):
-#         if naive_bayes_predict(my_text) > 0:
-#             # the predicted class is 1
-#             st.write('Your input text is positive')
-#         else:
-#             # otherwise the predicted class is 0
-#             st.write('Your input text is negative')
-
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
