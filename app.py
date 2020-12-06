c698c91935c2dbff4020823f3326dc4ef7453a55
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2020-12-06 19:41:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import streamlit as st
 # from utils import process_tweet, lookup, naive_bayes_predict
 
 
-def main():
-    #start the user interface
-    st.write("This is just a small text classification app. Type in your text below and don't forget to press the enter button before clicking/pressing the 'classify' button")
-    st.write("Don't fret if the prediction is not correct or if it is not what you expected, the model is not perfect.")
+st.write('a simpple app')
 
-    st.write("There is no provision for neutral text, yet...")
+# def main():
+#     #start the user interface
+#     st.write("This is just a small text classification app. Type in your text below and don't forget to press the enter button before clicking/pressing the 'classify' button")
+#     st.write("Don't fret if the prediction is not correct or if it is not what you expected, the model is not perfect.")
+
+#     st.write("There is no provision for neutral text, yet...")
 #     my_text = st.text_input("Enter the text you want to classify", "Change this...", max_chars=100, key='to_classify')
 
 #     if st.button('Classify', key='classify_button'):
@@ -18,5 +20,5 @@ def main():
 #             # otherwise the predicted class is 0
 #             st.write('Your input text is negative')
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
